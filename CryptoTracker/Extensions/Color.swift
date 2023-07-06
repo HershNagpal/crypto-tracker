9f9fd8f1338799678cc541d8e02a1060f620398c
Hersh Nagpal <hershnagpal@gmail.com>
2023-07-06 18:59:36 -0400

    Initial Commit

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Hersh Nagpal on 7/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
